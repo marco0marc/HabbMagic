3bd338f4db997e75b7587fc59c272b900e644357
marco0marc <35079354+marco0marc@users.noreply.github.com>
2018-01-03 21:33:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<html lang="en">
+  <head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Habbmagic</title>
+	<link rel="icon" href="magic-favicon.png" />
+<link rel="icon" type="image/png" href="magic-favicon.png" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content=" " />
+	<meta name="keywords" lang="en" content=" " xml:lang="en" />
+	<meta name="revisit-after" content="1 month" />
+	<meta name="author" content="Habbmagic" /> 
+	<meta name="rating" content="Safe For Kids" />
+	<link rel="stylesheet" type="text/css" href="g.css?v=0.0.3" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/fadeslideshow.js"></script>
+	<script type="text/javascript">
+	var mygallery=new fadeSlideShow({
+	wrapperid: "advert", //ID of blank DIV on page to house Slideshow
+	dimensions: [312, 83], //width/height of gallery in pixels. Should reflect dimensions of largest image
+	imagearray: [
+		["i/advert1.png", "javascript:ajaxpage('#', 'center_middle');", "", ""],
+		["i/advert2.png", "javascript:ajaxpage('#', 'center_middle');", "", ""]
+		 //<--no trailing comma after very last image element!
+	],
+	displaymode: {type:'auto', pause:2500, cycles:0, wraparound:false},
+	persist: false, //remember last viewed slide and recall within same session?
+	fadeduration: 500, //transition duration (milliseconds)
+	descreveal: "ondemand",
+	togglerid: ""
+	})
+	</script>
+  </head>
+
+  <body>
+
+<div class="mainHeader__wrapper">
+<div class="mainHeader__inner">
+<a href="https://openclassrooms.com/" class="mainHeader__logo">
+<span class="a11y">OpenClassrooms.com</span>
+</a>
+<div class="mainHeader__content">
+<div id="tab-main" class="mainHeader__tabs">
+<div class="mainHeader__container mainHeader__container--mainMenu">
+<nav role="navigation" aria-label="Menu principal">
+<ul class="mainTopNav">
+<li class="mainTopNav__itemContainer">
+<a class="mainTopNav__item" href="http://habbmagic.shost.ca/Generateur.html">Nos générateur</a>
+</li>
+<li class="mainTopNav__itemContainer">
+<a class="mainTopNav__item" href="">C</a>
+</li>
+<li class="mainTopNav__itemContainer">
+<a class="mainTopNav__item" href="https://openclassrooms.com/forum/">Forums</a>
+</li>
+<li class="mainTopNav__itemContainer">
+<a class="mainTopNav__item" href="https://openclassrooms.com/premium">Premium</a>
+</li>
+<li class="mainTopNav__itemContainer">
+<a class="mainTopNav__item u--colorBusiness" lang="en" href="https://openclassrooms.com/for-business">For Business</a>
+</li>
+</ul>
+</nav>
+</div>
+                    
+    </body>
+
+		<section class="timezonebox">
+			Timezone: GMT
+			<a class="fhinfo">Bienvenue<span class="admin">L'administration</span></a>
+		</section>
+	</section>
+</section>
+<section class="header">
+	<section class="wrapper">
+		<section class="towers">
+			<section class="logobox"><a href="/" class="logo"></a></section>
+			<section class="adverts" id="advert"></section>
+		</section>
+	</section>
+</section>
+<section class="bottombar">
+	<section class="wrapper">
+		<section class="djsays">
+			<b>L'équipe: Bienvenue sur Habbmagic.</b>
+		</section>
+		<section class="mininav">
+			<section class="hotel"></section>
+			<section class="alert"></section>
+			<section class="message"></section>
+			<section class="gaming"></section>
+		</section>
+	</section>
+</section>
+<section class="wrapper">
+	<section class="leftside">
+		<section class="leftbox">
+			<section class="left-top"></section>
+			<Section class="left-middle">
+			<body><br>
+<link rel="stylesheet" type="text/css" media="screen, projection, print" href="./style.css">
+
+			</section>
+			<Section class="left-bottom"></section>
+
+		<section class="leftbox">
+			<section class="left-top"></section>
+			<Section class="left-middle">
+			
+			</section>
+			<Section class="left-bottom"></section>
+		</section>
+	</section>
+	</section>
+	<section class="middleside"> <br /> <br />
+		<Section class="middlebox">
+			<Section class="middle-top"></section> 
+			<section class="middle-middle"> 
+HabbMagic ne fait en aucun cas partie de Habbo Hotel. 
+© Habbo Hotel, Habbo, Sulake et logos associés sont des marques déposées du groupe Sulake. Par conséquent, Sulake ne pourrait être tenu responsable des propos ou images diffusées sur ce site. 
+Copyright, des marques déposées et d'autres droits du matériel du Habbo Hôtel et d'autres services du groupe de Sulake sur ce site.
+			</section>
+			<section class="middle-bottom"></section>
+		</section>
+
+
+
+			</section>
+			<section class="middle-bottom"></section>
+		</section>
+	</section>
+	
+	<section class="rightside">
+		<section class="rightbox">
+			<section class="ipad">			
+				<section class="icontent">
+					</section>
+					<section class="imain">
+					
+						
+						<br />
+					</section>
+				</section>			
+			</section>
+		</section>
+	</section>
+</section>
+<br><br><br><br><br>
+<div class="footer">
+<div class="footertxt"><b>&copy; FlyHabbo</b><div style="float: right; margin: 0 40px; 0 0;"><b>Designed & Coded By marc &amp;  Habbo.FR </b></div></div></div>
+</body>
+</html>
